4ef85037339149f049890b47212e6a2ca6b70110
Kim Jay Hyun <jhkim@gridone.co.kr>
2024-02-15 23:37:53 +0900

    Making Modal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
+
+
+  <div class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
+  </div>
+
+
+
+
+
+
   <div class="menu">
       <a v-for="(a, i) in menus" :key="i" > {{ a }} </a>
       
   </div>
   
-  
+  <div v-for="(a, i) in products" :key="i">
+    <h4 class="red" :style="스타일"> {{ a }} </h4>
+    <p> {{ prices[i] }} 만원 </p>  
+    
+    <button @click="increase(i)">허위매물신고</button><span>신고수 : {{ votes[i] }}</span>
+  </div> 
+
+  <hr>
+
   <div>
+    <img src="./assets/room0.jpg" class="room-img">
     <h4 class="red" :style="스타일"> {{ products[0] }} </h4>
     <p> {{ price1 }} 만원 </p>
+    <!-- <button v-on:click="console.log('hi')">허위매물신고</button><span>신고수 : 0</span> -->
+    <!-- <button @click="신고수++">허위매물신고</button><span>신고수 : {{ 신고수 }}</span> -->
+    
+    <button @click="increase">허위매물신고</button><span>신고수 : {{ 신고수 }}</span>
   </div>
 
   <div>   
@@ -26,14 +53,24 @@ export default {
     return {
       price1 : 60,
       price2 : 70,
-      
+      신고수 : 0,
       // html attribute는 : 로 적용 가능
       스타일 : "color : blue",
 
       menus : ['Home', "Products", 'About'],
-      products : ['일원동', '이원동', '삼원동']
+      
+      products : ['일원동', '이원동', '삼원동'],
+      prices : [1, 2, 3],
+      votes : [0, 0, 0]
     }
   },
+
+  methods: {
+    increase(i) {
+      this.votes[i] ++;
+    }  
+  },
+
   components: {
     
   }
@@ -41,6 +78,12 @@ export default {
 </script>
 
 <style>
+.romm-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
